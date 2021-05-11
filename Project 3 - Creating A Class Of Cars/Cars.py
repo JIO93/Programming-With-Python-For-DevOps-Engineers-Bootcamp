ec9f1815fed069265533e56a26ffea5b3270f34f
JIO <javierx1993@gmail.com>
2021-05-10 22:32:03 -0700

    Added comments

--- Project 3 - Creating A Class Of Cars/Cars.py
+++ Project 3 - Creating A Class Of Cars/Cars.py
@@ -18,5 +18,8 @@ class Cars:
 #Class allows us to call the functions inside of a variable. You must specify objects so they become available to use.
 my_cars = Cars("Ford", "F150", "2020")
 
-#The line below uses the print_make_and_year method to display the make and year of our car
+#The line below uses the print_model method to display the Model of our car
+print(my_cars.print_model())
+
+#The line below uses the print_make_and_year method to display the make and year of our car. We have to specify the arguments as we don't specify the make or year in the Method.
 print(my_cars.print_make_and_year("F150", "2020"))
