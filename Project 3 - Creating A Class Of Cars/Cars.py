550e1bda84a17182ced440f6c174fe2971a443f4
JIO <javierx1993@gmail.com>
2021-05-10 22:23:25 -0700

    Adding Project 3 File

---
+++ Project 3 - Creating A Class Of Cars/Cars.py
@@ -0,0 +1,22 @@
+"""Method is what you call a function that is inside of a class.
+Below we are specifying the Methods for our class.
+"""
+
+class Cars:
+    def __init__(self, model, make, year):
+        self.model = "Ford"
+        self.make = make
+        self.year = year
+
+    def print_model(self):
+        print(self.model)
+
+    def print_make_and_year(self, make, year):
+        print(make)
+        print(year)
+
+#Class allows us to call the functions inside of a variable. You must specify objects so they become available to use.
+my_cars = Cars("Ford", "F150", "2020")
+
+#The line below uses the print_make_and_year method to display the make and year of our car
+print(my_cars.print_make_and_year("F150", "2020"))
