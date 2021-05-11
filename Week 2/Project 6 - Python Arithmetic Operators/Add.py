22dc12f0c5eb68cec2ca067ff1ff45167bda3428
Javier Ortiz <javierx1993@gmail.com>
2021-05-11 12:30:40 -0700

    Adding Project 6 Files

---
+++ Week 2/Project 6 - Python Arithmetic Operators/Add.py
@@ -0,0 +1,17 @@
+#line Below is simple Addition
+print(5 + 10)
+
+#Lines below specify Values for a letter and then subtracts them
+x = 5
+y = 10
+
+print(x - y)
+
+#The lines below specify values for x and y, define the addition function and prints it to the terminal (You can put whatever numbers you'd like after addition()) 
+x = 5
+y = 10
+
+def addition(x: int, y: int):
+    print(x + y)
+
+addition(10, 5)
